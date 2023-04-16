748581bc36dcaa42687d38fb948c72d1acb6d6c4
John Trady <docktor.mom19.93@gmail.com>
2023-04-16 18:11:30 +0600

    init commit

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@
 import '../js/theme-color.js';
 import '../js/swiper.js';
 
+
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		FastClick.attach(document.body);
+	}, false);
+}
